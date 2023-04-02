d1134ce5de7ee983f31e456d30ebf3872c859a16
junggeyy <122555491+junggeyy@users.noreply.github.com>
2023-04-01 23:41:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import './App.css';
+import React, { useState } from 'react';
+import request from 'browser-request';
 
-import React from 'react';
-import CatImage from './CatImage';
+function CatImage() {
+  const [imageUrl, setImageUrl] = useState(null);
 
-function App() {
+  const fetchCatImage = () => {
+    const apikey = 'e9df62b8-28ec-46e8-8db4-401d596f1231';
+    const url = 'https://api.thecatapi.com/v1/images/search';
+    const headers = {
+      'x-api-key': apikey
+    };
+    request({ url, headers, json: true }, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      } else if (!response.statusCode === 200) {
+        console.error(`HTTP error! Status: ${response.statusCode}`);
+      } else {
+        setImageUrl(body[0].url);
+      }
+    });
+  };
+  
+
   return (
     <div>
-      <h1>Random Cat Image</h1>
-      <CatImage />
+      {imageUrl ? (
+        <img src={imageUrl} alt="Random cat" />
+      ) : (
+        <p>No cat image yet</p>
+      )}
+      <button onClick={fetchCatImage}>Get Random Cat Image</button>
     </div>
   );
 }
 
-export default App;
+export default CatImage;
