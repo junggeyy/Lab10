513eaec54e009b39d9edd1802eea13d74ed93d26
junggeyy <122555491+junggeyy@users.noreply.github.com>
2023-04-01 23:16:06 -0400

    update for lab11

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import './App.css';
 
-import Container from './Container';
-import MyList from './ItemList';
-const items = [
-  { id: 1, name: 'Item 1' },
-  { id: 2, name: 'Item 2' },
-  { id: 3, name: 'Item 3' },
-];
-const myStyle = {
-  backgroundColor: 'blue',
-  color: 'white',
-  padding: '10px 20px',
-  border: 'none',
-  borderRadius: '5px',
-};
+import React from 'react';
+import CatImage from './CatImage';
+
 function App() {
   return (
-    <div className="App">
-      <header className='App-header'>
-       <h1>This is react app!</h1>
-       <Container style ={myStyle} />
-       <h1  style={{ fontWeight: 'italic' }}>My List</h1>
-       <MyList items={items} className ="list"/>
-      </header>
-     
+    <div>
+      <h1>Random Cat Image</h1>
+      <CatImage />
     </div>
   );
 }
